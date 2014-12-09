90442017676e1837b7336088a417a2d5637aead2
chentaichen <ct.chen@ischool.com.tw>
2014-12-09 18:33:05 +0800

    高中個人學期成績證明單(可選擇列印原始/擇優)

---
+++ SH_StudentRecordReport/SH_StudentRecordReport/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SH_StudentRecordReport
+{
+    public class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        //[MainMethod()]
+        [FISCA.MainMethod()]
+        static public void Main()
+        {
+            StudentRecordReportMain report = new StudentRecordReportMain();
+        }
+    }
+}
